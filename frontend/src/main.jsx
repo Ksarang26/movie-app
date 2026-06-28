5098e3aaa6da0e87101a010df0120350a24d551d
Ksarang26 <kulkarnisarang38@gmail.com>
2026-06-28 23:42:09 +0530

    feat: configure Redux Toolkit, RTK Query, and React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,31 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import store from './redux/store.js'
+import {Provider} from 'react-redux'
+import {Route, RouterProvider, createRoutesFromElements, createBrowserRouter} from 'react-router-dom'
+
+
+//Auth
+
+
+// Restricted
+
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} >
+      <Route index={true} path='/' element={<Home />} />
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <App />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider> 
   
 )
---
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import useState from 'react'
+import { 
+    AiOutlineHome,
+    AiOutlineLogin,
+    AiOutlineUserAdd,
+} from 'react-icons/ai'
+import {MdOutlineLocalMovies} from 'react-icons/md'
+
+const Navigation = () => {
+  return (
+    <div>
+      Navigation
+    </div>
+  )
+}
+
+export default Navigation
