67ebd3f2a082d4b608df781313c2dfa6b590412a
Ksarang26 <kulkarnisarang38@gmail.com>
2026-07-01 22:34:42 +0530

    registering user

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,12 +11,17 @@ import {Route, RouterProvider, createRoutesFromElements, createBrowserRouter} fr
 
 // Restricted
 
+import Login from './pages/Auth/Login.jsx'
+import Register from './pages/Auth/Register.jsx'
+
 import Home from './pages/Home.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />} >
       <Route index={true} path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 )
--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -50,7 +50,9 @@ const Navigation = () => {
             <span className='hidden nav-item-name mt-[3rem]'>SHOP</span>
           </Link>
         </div>
+
         {/* Section 2 */}
+
         <div className="relative">
           <button onClick={toggleDropdown} className='text-grey-800 focus:outline-none'>
             {userInfo ? (
@@ -62,8 +64,7 @@ const Navigation = () => {
             {userInfo && (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className={`h-4 w-4 ml-1 ${
-                  dropdownOpen ? "transform rotate-180" : ""
+                className={`h-4 w-4 ml-1 ${dropdownOpen ? "transform rotate-180" : ""
                   }`}
                 fill="none"
                 viewBox="0 0 24 24"
@@ -78,9 +79,67 @@ const Navigation = () => {
               </svg>
             )}
           </button>
+
+          {dropdownOpen && userInfo && (
+            <ul className={`absolute right-0 mt-2 mr-14 w-[10rem]
+              space-y-2 bg-white text-grey-600 
+              ${!userInfo.isAdmin ? "-top-20" : "-top-24"}`
+              }>
+
+
+              {userInfo.isAdmin && (
+                <>
+                  <li>
+                    <Link 
+                      to="/admin/movies/dashboard" 
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                </>
+              )}
+
+              <li>
+                <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
+                  Profile 
+                </Link>
+              </li>
+
+              <li>
+                <button
+                  onClick = {logouthandler}
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          )}
+
+          {!userInfo && (
+              <ul className="flex">
+                <li>
+                  <Link to='/login'
+                    className='flex items-center transition-transform
+                    transform hover:translate-x-2 ml-[1rem]'>
+                    <AiOutlineLogin className='mr-2 mt-[4px]' size={26} />
+                    <span className='hidden nav-item-name'>LOGIN</span>                                          
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to='/register'
+                    className='flex items-center transition-transform
+                    transform hover:translate-x-2 mb-[1rem] ml-[1rem]'>
+                    <AiOutlineUserAdd className='mr-2 mt-[4px]' size={26} />
+                    <span className='hidden nav-item-name'>REGISTER</span>                                          
+                  </Link>
+                </li>
+              </ul>
+          )}
+
         </div>
-
-
       </section>
     </div>
   )
